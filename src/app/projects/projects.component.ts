c9f574048ba07497a44ae1cb5cd57a0cf651bd9c
ahmed-salama07 <ahmed_salama99@outlook.com>
2025-10-27 21:26:49 +0300

    Fourth Push

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,42 +13,42 @@ export class ProjectsComponent {
   proj: Projects[] = [
     {
       title: 'To-DO List',
-      imgUrl: '/project1.png',
+      imgUrl: 'project1.png',
       projectUrl: 'https://ahmed-salama07.github.io/To-Do-List/',
     },
     {
       title: 'My CV',
-      imgUrl: '/project2.png',
+      imgUrl: 'project2.png',
       projectUrl: 'https://ahmed-salama07.github.io/Ahmed-Salama-CV/',
     },
     {
       title: 'Dashboard',
-      imgUrl: '/project3.png',
+      imgUrl: 'project3.png',
       projectUrl: 'https://ahmed-salama07.github.io/Bootstrap-Dashboard/',
     },
     {
       title: 'Bootstrap Practice',
-      imgUrl: '/project4.png',
+      imgUrl: 'project4.png',
       projectUrl: 'https://ahmed-salama07.github.io/BootStrap-Practice-1/',
     },
     {
       title: 'Restaurant Website',
-      imgUrl: '/project5.png',
+      imgUrl: 'project5.png',
       projectUrl: 'https://ahmed-salama07.github.io/Restaurant/',
     },
     {
       title: 'Iphone',
-      imgUrl: '/project6.png',
+      imgUrl: 'project6.png',
       projectUrl: 'https://ahmed-salama07.github.io/Challenge-2-FM/',
     },
     {
       title: 'CSS Practice',
-      imgUrl: '/project7.png',
+      imgUrl: 'project7.png',
       projectUrl: 'https://ahmed-salama07.github.io/Challenge-2-FM/',
     },
     {
       title: 'Leon Theme',
-      imgUrl: '/project8.png',
+      imgUrl: 'project8.png',
       projectUrl: 'https://ahmed-salama07.github.io/HTML-CSS-Practice/',
     },
   ]
